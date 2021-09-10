06e377297937ecd87e82e1220661aacf8a974048
Trindkr <maria.trindkaer@gmail.com>
2021-09-10 13:53:37 +0200

    Update RegExprTests.cs

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -4,5 +4,18 @@ namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+<<<<<<< Updated upstream
+=======
+        [Fact]
+        public void TestName()
+        {
+        //Given
+            var input = new string[]{"cover 4274 image,& and related `??","trade%/&% dress 2323 are trademarks"};
+        //When
+            var output = SplitLine(input);
+        //Then
+            Assert.Equal(new string[]{"cover","4274", "image","and","related","trade,","dress","2323","are","trademarks"},output);
+        }
+>>>>>>> Stashed changes
     }
 }
