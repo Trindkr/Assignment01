88712660c19fbf7a04d479a9ec7b94d0a9166702
00kristian <90248771+00kristian@users.noreply.github.com>
2021-09-13 20:17:09 +0200

    Reg ex (#1)
    * Update RegExprTests.cs
    
    * Added test for method SplitLine
    
    * Implemented Splitline function and test
    
    * Implementedresolution function and test
    
    * Implemented Innertext function and test
    
    Co-authored-by: Trindkr <maria.trindkaer@gmail.com>
    Co-authored-by: Kristian Berlin <krbj@itu.dk>

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,8 +1,68 @@
 using Xunit;
+using static Assignment1.RegExpr;
+using System;
+using System.Collections.Generic;
 
 namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+        [Fact]
+        public void Splitline_given_StringArray_return_only_numbers_and_Words()
+        {
+        //Given
+            var input = new string[]{"cover 4274 image,& and related `??", "trade%/&% dress 2323 are trademarks"};
+        //When 
+            var output = SplitLine(input);
+        //Then 
+            Assert.Equal(new string[]{ "cover","4274", "image","and","related","trade","dress","2323","are","trademarks"},output);
+        }
+
+        [Fact]
+        public void Resolution_given_stringArray_returning_tuples_of_int()
+        {
+        //Given
+        var input = new string[]{"1920x1080", "1024x768, 800x600, 640x480", "320x200, 320x240, 800x600", "1280x960"};
+
+        //When
+        var output = Resolution(input);
+        //Then
+        var tupleList = new List<(int i,int j)>
+        {
+             (1920,1080),
+             (1024,768),
+             (800,600),
+             (640, 480),
+             (320, 200),
+             (320, 240),
+             (800, 600),
+             (1280, 960)
+        };
+
+            Assert.Equal(tupleList, output);        
+        }
+
+
+        
+    [Fact]
+    public void TestName()
+    {
+    //Given
+    string input = @"<div>  <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=""/wiki/Theoretical_computer_science"" title=""Theoretical computer science"">theoretical computer science</a> and <a href=""/wiki/Formallanguage"" title=""Formal language"">formal language</a> theory, a sequence of <a href=""/wiki/Character(computing)"" title=""Character (computing)"">characters</a> that define a <i>search <a href=""/wiki/Pattern_matching"" title=""Pattern matching"">pattern</a></i>. Usually this pattern is then used by <a href=""/wiki/String_searchingalgorithm"" title=""String searching algorithm"">string searching algorithms</a> for ""find"" or ""find and replace"" operations on <a href=""/wiki/String(computer_science)"" title=""String (computer science)"">strings</a>.</p> </div>";
+   
+    string tag="a";
+    
+    //When
+    var output = InnerText(input,tag);
+
+    //Then
+    Assert.Equal( new string[]{"theoretical computer science","formal language","characters","pattern","string searching algorithms","strings"},output);
+    
     }
+    }
+
+
+
+    
+    
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using static System.Collections.IEnumerable;
+
 
 namespace Assignment1
 {
@@ -7,17 +11,43 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            
+            foreach (string item in lines)
+            {
+                string[] temp = Regex.Split(item, @"\W+\s*");
+                temp = temp.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                foreach  (string item2 in temp)
+                {
+                    yield return item2;
+                }
+                temp = null;
+            }
+
         }
 
-        public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+        public static IEnumerable<(int width, int height) > Resolution(IEnumerable<string>  resolutions)
         {
-            throw new NotImplementedException();
+            foreach (string item in resolutions)
+            {
+                string[] temp = Regex.Split(item, @"[,]");foreach  (string item2 in temp)
+                {
+                    string[] temp2 = Regex.Split(item2, @"[x]");
+                    
+                    int width = Convert.ToInt32( temp2[0]);
+                    int height = Convert.ToInt32( temp2[1]);    
+                    yield return (width, height);
+                
+                }
+                temp = null;
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
-        {
-            throw new NotImplementedException();
+        {            
+               foreach (Match match in Regex.Matches(html,@$"<{tag}( .+?)?>(?<Text>.+?)<\/{tag}>" ))
+               {
+                   yield return Regex.Replace(match.Groups["Text"].Value, @"<[^>]*>","");
+               }
         }
     }
 }
