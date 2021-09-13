7caf11857d4aa86efee2013c44d375faf78b238d
Kristian Berlin <krbj@itu.dk>
2021-09-13 19:03:44 +0200

    Implementedresolution function and test

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,19 +1,48 @@
 using Xunit;
 using static Assignment1.RegExpr;
+using System;
+using System.Collections.Generic;
 
 namespace Assignment1.Tests
 {
     public class RegExprTests
     {
         [Fact]
-        public void TestName()
+        public void Splitline_given_StringArray_return_only_numbers_and_Words()
         {
         //Given
-            var input = new string[]{"cover 4274 image,& and related `??"};
-        //When "trade%/&% dress 2323 are trademarks"
+            var input = new string[]{"cover 4274 image,& and related `??", "trade%/&% dress 2323 are trademarks"};
+        //When 
             var output = SplitLine(input);
-        //Then "trade","dress","2323","are","trademarks"
-            Assert.Equal(new string[]{ "cover","4274", "image","and","related"},output);
+        //Then 
+            Assert.Equal(new string[]{ "cover","4274", "image","and","related","trade","dress","2323","are","trademarks"},output);
+        }
+
+        [Fact]
+        public void Resolution_given_stringArray_returning_tuples_of_int()
+        {
+        //Given
+        var input = new string[]{"1920x1080", "1024x768, 800x600, 640x480", "320x200, 320x240, 800x600", "1280x960"};
+
+        //When
+        var output = Resolution(input);
+        //Then
+        var tupleList = new List<(int i,int j)>
+        {
+             (1920,1080),
+             (1024,768),
+             (800,600),
+             (640, 480),
+             (320, 200),
+             (320, 240),
+             (800, 600),
+             (1280, 960)
+        };
+
+            Assert.Equal(tupleList, output);        
         }
     }
+
+    
+    
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Linq;
+using static System.Collections.IEnumerable;
 
 namespace Assignment1
 {
@@ -25,9 +26,27 @@ namespace Assignment1
 
         }
 
-        public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+        public static IEnumerable<(int width, int height) > Resolution(IEnumerable<string>  resolutions)
         {
-            throw new NotImplementedException();
+            foreach (string item in resolutions)
+            {
+                string[] temp = Regex.Split(item, @"[,]");
+
+                foreach  (string item2 in temp)
+                {
+                    string[] temp2 = Regex.Split(item2, @"[x]");
+                    
+                    int width = Convert.ToInt32( temp2[0]);
+                    int height = Convert.ToInt32( temp2[1]);
+
+                    
+                    yield return (width, height);
+                
+                }
+
+                
+                temp = null;
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
